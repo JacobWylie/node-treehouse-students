63e42647a944d5e360fa62d3d89f5b8429f15ed8
JacobWylie <jacobwylie@gmail.com>
2017-03-08 14:56:33 +0100

    populated user info from treehouse json file and error message when user not found

--- router.js
+++ router.js
@@ -1,3 +1,5 @@
+const Profile = require("./profile.js");
+
 // Handle HTTP route GET / and POST / i.e. Home
 const home = (request, response) => {
 	// if url == "/" && GET
@@ -21,13 +23,35 @@ const user = (request, response) => {
 		response.statusCode = 200;
   	response.setHeader('Content-Type', 'text/plain');
   	response.write('Header\n');
-  	response.write(username + '\n');  	
-  	response.end('Footer\n');
-		// get json from Treehouse
-			// on 'end'
+
+  	// Get json from Treehouse
+  	let studentProfile = new Profile(username);
+			// on end:
+			// When the JSON body is fully recieved the 
+			// the "end" event is triggered and the full body
+			// is given to the handler or callback
+			studentProfile.on("end", profileJSON => {
 				// show profile
-			// on error
-				// show error
+
+				// Store the values which we need
+				let values = {
+					avatarUrl: profileJSON.gravatar_url,
+					username: profileJSON.profile_name,
+					badges: profileJSON.badges.length,
+					javaScriptPoints: profileJSON.points.JavaScript
+				}
+				// Simple response
+  			response.write(`${values.username} has ${values.badges} badges\n`);  	
+  			response.end('Footer\n');
+			});
+			// on error:
+			// If a parsing, network or HTTP error occurs an
+			// error object is passed in to the handler or callback
+			studentProfile.on("error", error => {
+				// Show error 	
+				response.write(error.message + '\n');
+  			response.end('Footer\n');
+			});
 	}
 }
 
